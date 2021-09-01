165afb12664a5ab5040c53af330189b76b079276
Sasha <sashabazhura123@gmail.com>
2021-09-01 12:25:42 +0200

    pt2

---
+++ js/toggler.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const flexopener = document.getElementsByClassName('flex-helper1')[0];
+const navbarNav = document.getElementsByClassName('navbar-navHelper')[0];
+const navbarBrand = document.getElementsByClassName('navbar-brandHelper')[0];
+
+toggleButton.addEventListener('click', () => {
+	
+	flexopener.classList.toggle('list-menu');
+	
+	navbarNav.classList.toggle('mg10');
+	navbarBrand.classList.toggle('mg20');
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+})
+	
